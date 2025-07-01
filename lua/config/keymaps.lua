10616c417c21f97eacfd05dc13ab5cedcfd3637b
Sanchit Rk <sanchitrrk@gmail.com>
2025-07-01 18:37:41 +0530

    keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,5 +2,26 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.g.mapleader = " "
+-- Open NetRW
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open NetRW" })
+
+-- Move selected lines up and down in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Keep the cursor centered when jumping or searching
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Paste over visual selection without yanking it
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- Yank to system clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- Delete to the void register (uses a non-conflicting key)
+vim.keymap.set({ "n", "v" }, "<leader>D", [["_d]], { desc = "Delete to Void Register" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,3 +6,17 @@ vim.g.python_host_prog = "/Users/sanchitrk/bin/neovenv/bin/python"
 vim.g.python3_host_prog = "/Users/sanchitrk/bin/neovenv/bin/python"
 
 vim.g.lazyvim_picker = "telescope"
+
+--
+-- from ThePrimegean
+--
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.hlsearch = false
+vim.opt.isfname:append("@-@")
+vim.opt.colorcolumn = "100"
