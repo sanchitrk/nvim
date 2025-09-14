61200ee086a2cc06680ba2b785963cc5a1c5f540
Sanchit Rk <sanchitrrk@gmail.com>
2025-09-14 19:09:56 +0530

    add support for go with go.lua

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,55 +1,23 @@
-return {}
--- return {
---   -- Go language server
---   {
---     "neovim/nvim-lspconfig",
---     opts = {
---       servers = {
---         gopls = {
---           settings = {
---             gopls = {
---               analyses = {
---                 unusedparams = true,
---               },
---               staticcheck = true,
---               gofumpt = true,
---             },
---           },
---         },
---       },
---     },
---   },
---
---   -- Go-specific tools
---   {
---     "ray-x/go.nvim",
---     dependencies = {
---       "ray-x/guihua.lua",
---       "neovim/nvim-lspconfig",
---       "nvim-treesitter/nvim-treesitter",
---     },
---     config = function()
---       require("go").setup()
---     end,
---     event = { "CmdlineEnter" },
---     ft = { "go", "gomod" },
---   },
---
---   -- Ensure Go treesitter parser is installed
---   {
---     "nvim-treesitter/nvim-treesitter",
---     opts = function(_, opts)
---       vim.list_extend(opts.ensure_installed, { "go", "gomod", "gosum" })
---     end,
---   },
---
---   -- Go formatter and linter
---   {
---     "stevearc/conform.nvim",
---     opts = {
---       formatters_by_ft = {
---         go = { "goimports", "gofumpt" },
---       },
---     },
---   },
--- }
+return {
+    "ray-x/go.nvim",
+    dependencies = { -- optional packages
+    "ray-x/guihua.lua", "neovim/nvim-lspconfig", "nvim-treesitter/nvim-treesitter"},
+    opts = {
+        -- lsp_keymaps = false,
+        -- other options
+    },
+    config = function(lp, opts)
+        require("go").setup(opts)
+        local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            pattern = "*.go",
+            callback = function()
+                require('go.format').goimports()
+            end,
+            group = format_sync_grp
+        })
+    end,
+    event = {"CmdlineEnter"},
+    ft = {"go", 'gomod'},
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+}
