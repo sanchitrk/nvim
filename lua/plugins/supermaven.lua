6f1ab1ab8dcd4a4356baaf28df027135461551c6
Sanchit Rk <sanchitrrk@gmail.com>
2025-11-19 12:30:43 +0530

    add supermaven remove copilot

---
+++ lua/plugins/supermaven.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "supermaven-inc/supermaven-nvim",
+    config = function()
+      require("supermaven-nvim").setup({
+        ignore_filetypes = { "markdown", "gitcommit" },
+      })
+    end,
+  },
+}
