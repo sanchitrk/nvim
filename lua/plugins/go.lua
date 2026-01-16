eba1987728856e81853b899e59a79701fbd480ba
Sanchit Rk <sanchitrrk@gmail.com>
2026-01-16 23:46:57 +0530

    added rust + go improvements

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,23 +1,54 @@
 return {
+  -- Go support
+  {
     "ray-x/go.nvim",
-    dependencies = { -- optional packages
-    "ray-x/guihua.lua", "neovim/nvim-lspconfig", "nvim-treesitter/nvim-treesitter"},
-    opts = {
-        -- lsp_keymaps = false,
-        -- other options
+    dependencies = {
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
     },
-    config = function(lp, opts)
-        require("go").setup(opts)
-        local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            pattern = "*.go",
-            callback = function()
-                require('go.format').goimports()
-            end,
-            group = format_sync_grp
-        })
+    config = function()
+      require("go").setup()
     end,
-    event = {"CmdlineEnter"},
-    ft = {"go", 'gomod'},
-    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+    build = ':lua require("go.install").update_all_sync()',
+  },
+
+  -- Treesitter support
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "go", "gomod", "gowork", "gosum" })
+    end,
+  },
+
+  -- Formatting
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        go = { "goimports", "gofumpt" },
+      },
+    },
+  },
+
+  -- DAP for debugging
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    dependencies = {
+      {
+        "mason-org/mason.nvim",
+        opts = function(_, opts)
+          opts.ensure_installed = opts.ensure_installed or {}
+          vim.list_extend(opts.ensure_installed, { "delve" })
+        end,
+      },
+      {
+        "leoluz/nvim-dap-go",
+        config = true,
+      },
+    },
+  },
 }
--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,106 +1,103 @@
-return {}
+return {
+  -- Rust language server
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        rust_analyzer = {
+          settings = {
+            ["rust-analyzer"] = {
+              cargo = {
+                allFeatures = true,
+                loadOutDirsFromCheck = true,
+                runBuildScripts = true,
+              },
+              checkOnSave = {
+                allFeatures = true,
+                command = "clippy",
+                extraArgs = { "--no-deps" },
+              },
+              procMacro = {
+                enable = true,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
 
---
--- return {
---   -- Rust language server
---   {
---     "neovim/nvim-lspconfig",
---     opts = {
---       servers = {
---         rust_analyzer = {
---           settings = {
---             ["rust-analyzer"] = {
---               cargo = {
---                 allFeatures = true,
---                 loadOutDirsFromCheck = true,
---                 runBuildScripts = true,
---               },
---               checkOnSave = {
---                 allFeatures = true,
---                 command = "clippy",
---                 extraArgs = { "--no-deps" },
---               },
---               procMacro = {
---                 enable = true,
---               },
---             },
---           },
---         },
---       },
---     },
---   },
---
---   -- Enhanced Rust support
---   {
---     "mrcjkb/rustaceanvim",
---     version = "^4",
---     ft = { "rust" },
---     opts = {
---       server = {
---         on_attach = function(client, bufnr)
---           -- Custom keymaps for Rust
---           vim.keymap.set("n", "<leader>cR", function()
---             vim.cmd.RustLsp("codeAction")
---           end, { desc = "Code Action", buffer = bufnr })
---           vim.keymap.set("n", "<leader>dr", function()
---             vim.cmd.RustLsp("debuggables")
---           end, { desc = "Rust Debuggables", buffer = bufnr })
---         end,
---         default_settings = {
---           ["rust-analyzer"] = {
---             cargo = {
---               allFeatures = true,
---             },
---           },
---         },
---       },
---     },
---     config = function(_, opts)
---       vim.g.rustaceanvim = vim.tbl_deep_extend("keep", vim.g.rustaceanvim or {}, opts or {})
---     end,
---   },
---
---   -- Crates.nvim for Cargo.toml management
---   {
---     "saecki/crates.nvim",
---     event = { "BufRead Cargo.toml" },
---     opts = {
---       completion = {
---         cmp = { enabled = true },
---       },
---     },
---   },
---
---   -- Treesitter support
---   {
---     "nvim-treesitter/nvim-treesitter",
---     opts = function(_, opts)
---       vim.list_extend(opts.ensure_installed, { "rust", "toml" })
---     end,
---   },
---
---   -- Formatting
---   {
---     "stevearc/conform.nvim",
---     opts = {
---       formatters_by_ft = {
---         rust = { "rustfmt" },
---       },
---     },
---   },
---
---   -- DAP for debugging
---   {
---     "mfussenegger/nvim-dap",
---     optional = true,
---     dependencies = {
---       {
---         "williamboman/mason.nvim",
---         opts = function(_, opts)
---           opts.ensure_installed = opts.ensure_installed or {}
---           vim.list_extend(opts.ensure_installed, { "codelldb" })
---         end,
---       },
---     },
---   },
--- }
+  -- Enhanced Rust support
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^4",
+    ft = { "rust" },
+    opts = {
+      server = {
+        on_attach = function(client, bufnr)
+          -- Custom keymaps for Rust
+          vim.keymap.set("n", "<leader>cR", function()
+            vim.cmd.RustLsp("codeAction")
+          end, { desc = "Code Action", buffer = bufnr })
+          vim.keymap.set("n", "<leader>dr", function()
+            vim.cmd.RustLsp("debuggables")
+          end, { desc = "Rust Debuggables", buffer = bufnr })
+        end,
+        default_settings = {
+          ["rust-analyzer"] = {
+            cargo = {
+              allFeatures = true,
+            },
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      vim.g.rustaceanvim = vim.tbl_deep_extend("keep", vim.g.rustaceanvim or {}, opts or {})
+    end,
+  },
+
+  -- Crates.nvim for Cargo.toml management
+  {
+    "saecki/crates.nvim",
+    event = { "BufRead Cargo.toml" },
+    opts = {
+      completion = {
+        cmp = { enabled = true },
+      },
+    },
+  },
+
+  -- Treesitter support
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "rust", "toml" })
+    end,
+  },
+
+  -- Formatting
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        rust = { "rustfmt" },
+      },
+    },
+  },
+
+  -- DAP for debugging
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    dependencies = {
+      {
+        "mason-org/mason.nvim",
+        opts = function(_, opts)
+          opts.ensure_installed = opts.ensure_installed or {}
+          vim.list_extend(opts.ensure_installed, { "codelldb" })
+        end,
+      },
+    },
+  },
+}
